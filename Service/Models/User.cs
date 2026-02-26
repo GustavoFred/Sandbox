92431d53bbefe4956600edc79494567d9665336b
Gu Fred <gustavofrederico99@gmail.com>
2026-02-26 20:23:32 -0300

    Added migrations and database connection

--- Service/Models/User.cs
+++ Service/Models/User.cs
@@ -3,9 +3,10 @@
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; } = null!;
+        public string SecondName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
         public bool PersonType { get; set; }
     }
 }
